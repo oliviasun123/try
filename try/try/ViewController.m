36d94730190f576b7e7dbb625c5fa738a0f988c6
oliviasun123 <sunyimind@126.com>
2016-09-13 21:37:10 +0800

    final

--- try/try/ViewController.m
+++ try/try/ViewController.m
@@ -22,16 +22,16 @@
     [pic setImage:[UIImage imageNamed:@"people"]];
     [self.view addSubview:pic];
     */
-    UILabel * label1 = [[UILabel alloc]initWithFrame:CGRectMake(Screen_w/2-100, Screen_h/2-120, 200, 200)];
+    UILabel * label1 = [[UILabel alloc]initWithFrame:CGRectMake(Screen_w/2-100, Screen_h/2-80, 200, 200)];
     label1.text = @"哈哈哈哈哈哈哈哈哈哈";
     label1.font = [UIFont systemFontOfSize:18];
     label1.textColor = [UIColor blackColor];
     [self.view addSubview:label1];
     
-    UILabel * label2 = [[UILabel alloc]initWithFrame:CGRectMake(Screen_w/2-60, Screen_h/2-150, 400, 200)];
-    label2.text = @"爸爸好！";
-    label2.font = [UIFont systemFontOfSize:25];
-    label2.textColor = [UIColor blackColor];
+    UILabel * label2 = [[UILabel alloc]initWithFrame:CGRectMake(Screen_w/2-130, Screen_h/2-130, 400, 200)];
+    label2.text = @"爸爸好!";
+    label2.font = [UIFont systemFontOfSize:75];
+    label2.textColor = [UIColor blueColor];
     [self.view addSubview:label2];
     // Do any additional setup after loading the view, typically from a nib.
 }
